33bb508fe9d6a0f5d4929ad0ecf47366f862f350
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-20 11:51:50 +0800

    Add files via upload

--- GMT-11-script-JKf-tectonics-World.sh
+++ GMT-11-script-JKf-tectonics-World.sh
@@ -1,7 +1,8 @@
 # Purpose: World tectonic map in Eckert IV equal-area pseudocylindrical projection
 # here: centered Pacific Ocean (180 grad)
-# GMT modules: pscoast, psxy, logo
-ps=GMT_tectonics_World
+# GMT modules: pscoast, psxy, pstext, psbasemap, logo, psconvert
+# Step-1. Add coastline map
+ps=GMT_tect_World.ps
 gmt pscoast -Rg -JKf180/9i -Gpapayawhip -Sazure -W0.25p -Dc \
     --FORMAT_GEO_MAP=dddF \
     --MAP_FRAME_PEN=dimgray \
@@ -13,7 +14,7 @@ gmt pscoast -Rg -JKf180/9i -Gpapayawhip -Sazure -W0.25p -Dc \
     --FONT_LABEL=8p,Helvetica,dimgray \
     -B+t"Tectonic plates boundaries: World map" -Bx30g30 -By10g10 \
     -Vv -K > $ps
-# Add tectonic plates
+# Step-2. Add tectonic plates
 gmt psxy -R -J TP_Pacific.txt -L -Wthick,red -O -K >> $ps
 gmt psxy -R -J TP_Philippine_Sea.txt -L -Wthick,green1 -O -K >> $ps
 gmt psxy -R -J TP_African.txt -L -Wthick,turquoise1 -O -K >> $ps
@@ -31,7 +32,7 @@ gmt psxy -R -J TP_Scotia.txt -L -Wthick,chartreuse1 -O -K >> $ps
 gmt psxy -R -J TP_South_Am.txt -L -Wthick,maroon1 -O -K >> $ps
 gmt psxy -R -J TP_Somali.txt -L -Wthickest,red -O -K >> $ps
 gmt psxy -R -J TP_Okhotsk.txt -L -Wthickest,red -O -K >> $ps
-# Step-9. Add text labels
+# Step-3. Add text labels
 echo "-160 7 PA" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
 echo "130 20 PS" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
 echo "15 15 AF" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
@@ -49,10 +50,12 @@ echo "-55 -56 SC" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
 echo "-58 -10 SA" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
 echo "50 -10 SO" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
 echo "150 55 OK" | gmt pstext -R -J -F+jTL+f10p,Times-Roman,black -O -K >> $ps
-# Add scale bar
+# Step-4. Add scale bar
 gmt psbasemap -R -J \
 --FONT=8p,Palatino-Roman,dimgray \
     -Lx11.8c/-1.3c+c50+w10000k+l"Eckert IV equal-area pseudocylindrical projection. Scale (km)"+f \
     -UBL/45p/-40p -O -K >> $ps
-# Add logo
+# Step-5. Add logo
 gmt logo -R -J -Dx10.2/-2.7+o0.1i/0.1i+w2c -O >> $ps
+# Step-6. Convert to image file using GhostScript (landscape orientation, 720 dpi)
+gmt psconvert GMT_tect_World.ps -A0.2c -E720 -Tj -Z
